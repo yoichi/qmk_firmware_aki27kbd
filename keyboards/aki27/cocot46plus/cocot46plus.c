umemotoryo/qmk_firmware

commit e6074e96357eca4be6104592224453ba5dbd7a19

update to QMK 0.27.9

keyboards/aki27/cocot46plus/cocot46plus.c
@@ -15,13 +15,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "quantum.h"
+#include QMK_KEYBOARD_H
 #include <math.h>
-#include "cocot46plus.h"
-#include "wait.h"
-#include "debug.h"
-#include <stdio.h>
-
 
 
 // Invert vertical scroll direction
@@ -65,10 +60,6 @@ uint16_t angle_array[] = COCOT_ROTATION_ANGLE;
 bool     BurstState        = false;  // init burst state for Trackball module
 uint16_t MotionStart       = 0;      // Timer for accel, 0 is resting state
 
-// Scroll Accumulation
-static int16_t h_acm       = 0;
-static int16_t v_acm       = 0;
-
 
 void pointing_device_init_kb(void) {
     // set the CPI.
@@ -78,75 +69,92 @@ void pointing_device_init_kb(void) {
 
 
 report_mouse_t pointing_device_task_kb(report_mouse_t mouse_report) {
+    static float x_accumulator = 0.0;
+    static float y_accumulator = 0.0;
+    static float prev_x = 0.0, prev_y = 0.0;
 
-    double rad = angle_array[cocot_config.rotation_angle] * (M_PI / 180) * -1;
-    int8_t x_rev =  + mouse_report.x * cos(rad) - mouse_report.y * sin(rad);
-    int8_t y_rev =  + mouse_report.x * sin(rad) + mouse_report.y * cos(rad);
+    float sensitivity = 0.5;           // Movement sensitivity
+    float smoothing_factor = 0.7;     // Smoothing factor
+    float sensitivity_multiplier = 1.5; // Sensitivity adjustment multiplier
 
+    // Apply rotation angle adjustment
+    double rad = (double)angle_array[cocot_config.rotation_angle] * (M_PI / 180) * -1;
+    float rotated_x = mouse_report.x * cos(rad) - mouse_report.y * sin(rad);
+    float rotated_y = mouse_report.x * sin(rad) + mouse_report.y * cos(rad);
+
+    // Apply smoothing to the rotated values
+    float smoothed_x = prev_x * smoothing_factor + rotated_x * (1.0 - smoothing_factor);
+    float smoothed_y = prev_y * smoothing_factor + rotated_y * (1.0 - smoothing_factor);
+    prev_x = smoothed_x;
+    prev_y = smoothed_y;
+
+    // Apply sensitivity multiplier
+    smoothed_x *= sensitivity_multiplier;
+    smoothed_y *= sensitivity_multiplier;
+
+    // Scroll mode handling
     if (cocot_get_scroll_mode()) {
-        // rock scroll direction
-        if (abs(x_rev) > abs(y_rev)) {
-            y_rev = 0;
+        static int h_acm = 0, v_acm = 0;
+
+        // Determine scroll direction
+        if (abs((int)smoothed_x) > abs((int)smoothed_y)) {
+            smoothed_y = 0; // Horizontal scroll
         } else {
-            x_rev = 0;
+            smoothed_x = 0; // Vertical scroll
         }
 
-        // accumulate scroll
-        h_acm += x_rev * cocot_config.scrl_inv;
-        v_acm += y_rev * cocot_config.scrl_inv * -1;
-
-        int8_t h_rev = h_acm >> scrl_div_array[cocot_config.scrl_div];
-        int8_t v_rev = v_acm >> scrl_div_array[cocot_config.scrl_div];
-
-        // clear accumulated scroll on assignment
-
-        if (h_rev != 0) {
-            if (mouse_report.h + h_rev > 127) {
-                h_rev = 127 - mouse_report.h;
-            } else if (mouse_report.h + h_rev < -127) {
-                h_rev = -127 - mouse_report.h;
-            }
-            mouse_report.h += h_rev;
-            h_acm -= h_rev << scrl_div_array[cocot_config.scrl_div];
-        }
-        if (v_rev != 0) {
-            if (mouse_report.v + v_rev > 127) {
-                v_rev = 127 - mouse_report.v;
-            } else if (mouse_report.v + v_rev < -127) {
-                v_rev = -127 - mouse_report.v;
-            }
-            mouse_report.v += v_rev;
-            v_acm -= v_rev << scrl_div_array[cocot_config.scrl_div];
+        // Accumulate scroll values
+        if (cocot_config.scrl_inv) {
+            h_acm += smoothed_x;
+            v_acm -= smoothed_y;
+        } else {
+            h_acm -= smoothed_x;
+            v_acm += smoothed_y;
         }
 
+        // Calculate scroll values with division factor
+        int8_t h_scroll = h_acm >> scrl_div_array[cocot_config.scrl_div];
+        int8_t v_scroll = v_acm >> scrl_div_array[cocot_config.scrl_div];
+
+        // Apply scroll to mouse report
+        if (h_scroll != 0) {
+            mouse_report.h += h_scroll;
+            h_acm -= h_scroll << scrl_div_array[cocot_config.scrl_div];
+        }
+        if (v_scroll != 0) {
+            mouse_report.v += v_scroll;
+            v_acm -= v_scroll << scrl_div_array[cocot_config.scrl_div];
+        }
+
+        // Reset X/Y movement in scroll mode
         mouse_report.x = 0;
         mouse_report.y = 0;
     } else {
-        mouse_report.x = x_rev;
-        mouse_report.y = y_rev;
+        // Movement smoothing and accumulation for normal mode
+        x_accumulator += smoothed_x * sensitivity;
+        y_accumulator += smoothed_y * sensitivity;
+        if (fabs(x_accumulator) >= 1.0) {
+            mouse_report.x = (int8_t)x_accumulator;
+            x_accumulator -= mouse_report.x;
+        } else {
+            mouse_report.x = 0;
+        }
+        if (fabs(y_accumulator) >= 1.0) {
+            mouse_report.y = (int8_t)y_accumulator;
+            y_accumulator -= mouse_report.y;
+        } else {
+            mouse_report.y = 0;
+        }
     }
 
     return pointing_device_task_user(mouse_report);
 }
 
-
-
 bool process_record_kb(uint16_t keycode, keyrecord_t* record) {
     // xprintf("KL: kc: %u, col: %u, row: %u, pressed: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed);
     
-    if (!process_record_user(keycode, record)) return false;
-
-    switch (keycode) {
-#ifndef MOUSEKEY_ENABLE
-        // process KC_MS_BTN1~8 by myself
-        // See process_action() in quantum/action.c for details.
-        case KC_MS_BTN1 ... KC_MS_BTN8: {
-            extern void register_button(bool, enum mouse_buttons);
-            register_button(record->event.pressed, MOUSE_BTN_MASK(keycode - KC_MS_BTN1));
-            return false;
-        }
-#endif
-
+    if (!process_record_user(keycode, record)) {
+        return false;
     }
 
     if (keycode == CPI_SW && record->event.pressed) {
@@ -159,7 +167,7 @@ bool process_record_kb(uint16_t keycode, keyrecord_t* record) {
         cocot_config.scrl_div = (cocot_config.scrl_div + 1) % SCRL_DIV_SIZE;
         eeconfig_update_kb(cocot_config.raw);
     }
-    
+
     if (keycode == ROT_R15 && record->event.pressed) {
         cocot_config.rotation_angle = (cocot_config.rotation_angle + 1) % ANGLE_SIZE;
         eeconfig_update_kb(cocot_config.raw);
@@ -171,21 +179,24 @@ bool process_record_kb(uint16_t keycode, keyrecord_t* record) {
     }
 
     if (keycode == SCRL_IN && record->event.pressed) {
-        cocot_config.scrl_inv = - cocot_config.scrl_inv;
+        cocot_config.scrl_inv = -cocot_config.scrl_inv;
         eeconfig_update_kb(cocot_config.raw);
     }
 
     if (keycode == SCRL_TO && record->event.pressed) {
-        { cocot_config.scrl_mode ^= 1; }
+        {
+            cocot_config.scrl_mode ^= 1;
+        }
     }
 
     if (keycode == SCRL_MO) {
-        { cocot_config.scrl_mode ^= 1; }
+        {
+            cocot_config.scrl_mode ^= 1;
+        }
     }
 
     return true;
 }
-
 
 void eeconfig_init_kb(void) {
     cocot_config.cpi_idx = COCOT_CPI_DEFAULT;

keyboards/aki27/cocot46plus/cocot46plus.h
@@ -19,27 +19,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "quantum.h"
 
-#define LAYOUT( \
-    A00, A01, A02, A03, A04, A05,                 B05, B04, B03, B02, B01, B00, \
-    A10, A11, A12, A13, A14, A15,                 B15, B14, B13, B12, B11, B10, \
-    A20, A21, A22, A23, A24, A25,                 B25, B24, B23, B22, B21, B20, \
-              A30, A31, A32, A33, A35,      B35,  B33, B32, B31, B30, \
-                                  A42, A34, A45,  B42, B34, B45 \
-   ) \
-  { \
-    { A00,   A01,   A02,   A03,   A04,   A05 }, \
-    { A10,   A11,   A12,   A13,   A14,   A15 }, \
-    { A20,   A21,   A22,   A23,   A24,   A25 }, \
-    { A30,   A31,   A32,   A33,   A34,   A35 }, \
-    { KC_NO, KC_NO, A42, KC_NO, KC_NO,   A45 }, \
-    { B00,   B01,   B02,   B03,   B04,   B05 }, \
-    { B10,   B11,   B12,   B13,   B14,   B15 }, \
-    { B20,   B21,   B22,   B23,   B24,   B25 }, \
-    { B30,   B31,   B32,   B33,   B34,   B35 },  \
-    { KC_NO, KC_NO, B42, KC_NO, KC_NO,   B45 }  \
-  }
-
-
 typedef union {
     uint32_t raw;
     struct {
@@ -52,10 +31,6 @@ typedef union {
     };
 } cocot_config_t;
 
-
-
-extern cocot_config_t cocot_config;
-
 enum cocot_keycodes {
 
     COCOT_SAFE_RANGE = SAFE_RANGE,
@@ -65,20 +40,19 @@ enum cocot_keycodes {
     ROT_L15,
     SCRL_MO,
     SCRL_TO,
-    SCRL_IN,
-
+    SCRL_IN
 };
 
-#define CPI_SW USER00
-#define SCRL_SW USER01
-#define ROT_R15 USER02
-#define ROT_L15 USER03
-#define SCRL_MO USER04
-#define SCRL_TO USER05
-#define SCRL_IN USER06
+#define CPI_SW QK_KB_0
+#define SCRL_SW QK_KB_1
+#define ROT_R15 QK_KB_2
+#define ROT_L15 QK_KB_3
+#define SCRL_MO QK_KB_4
+#define SCRL_TO QK_KB_5
+#define SCRL_IN QK_KB_6
 
-bool encoder_update_user(uint8_t index, bool clockwise);
-bool encoder_update_kb(uint8_t index, bool clockwise);
+extern cocot_config_t cocot_config;
+
 bool cocot_get_scroll_mode(void);
 void cocot_set_scroll_mode(bool mode);
 

keyboards/aki27/cocot46plus/config.h
@@ -17,65 +17,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#include "config_common.h"
-
-/* USB Device descriptor parameter */
-#define VENDOR_ID    0x1727
-#define PRODUCT_ID   0x0003
-#define DEVICE_VER   0x0001
-#define MANUFACTURER aki27
-#define PRODUCT      cocot46plus
-
-/* VIA */
 #define DYNAMIC_KEYMAP_LAYER_COUNT 7
 
 /* key matrix size */
 #define MATRIX_ROWS 10
 #define MATRIX_COLS 6
-
-// wiring of each half
-#define MATRIX_ROW_PINS { D4, C6, D7, E6, NO_PIN }
-#define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3 }
-
-/* Set 0 if debouncing isn't needed */
-#define DEBOUNCE 5
-
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#undef LOCKING_SUPPORT_ENABLE
-/* Locking resynchronize hack */
-#undef LOCKING_RESYNC_ENABLE
-
-/* Limit the maximum layer number to 8 */
-// #define LAYER_STATE_8BIT
-
-/* Rotary encoder */
-#define ENCODERS_PAD_A { D3 }
-#define ENCODERS_PAD_B { D2 }
-#define ENCODER_RESOLUTION 4
-
-#define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 200
-
-/* ws2812 RGB LED */
-#define RGB_DI_PIN B6
-
-#ifdef RGBLIGHT_ENABLE
-    #undef RGBLED_NUM
-    //#define RGBLIGHT_ANIMATIONS
-    // select individual effect 
-    #define RGBLIGHT_EFFECT_BREATHING
-    #define RGBLIGHT_EFFECT_RAINBOW_MOOD
-    #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-    #define RGBLIGHT_EFFECT_SNAKE
-    #define RGBLIGHT_EFFECT_STATIC_GRADIENT
-    #define RGBLIGHT_EFFECT_TWINKLE
-
-    #define RGBLED_NUM 12
-    #define RGBLIGHT_LIMIT_VAL 120
-    #define RGBLIGHT_HUE_STEP 10
-    #define RGBLIGHT_SAT_STEP 17
-    #define RGBLIGHT_VAL_STEP 17
-#endif
 
 #define ADNS5050_SCLK_PIN         B2
 #define ADNS5050_SDIO_PIN         B4

keyboards/aki27/cocot46plus/keymaps/default/keymap.c
@@ -32,8 +32,8 @@ enum layer_number {
 };
 
 
-#define LW_MHEN LT(1,KC_MHEN)  // lower
-#define RS_HENK LT(2,KC_HENK)  // raise
+#define LW_MHEN LT(1,KC_INT5)  // lower
+#define RS_HENK LT(2,KC_INT4)  // raise
 #define DEL_ALT ALT_T(KC_DEL)
 
 /*
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                          KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_MINS,
   //|-------------------------------------------------------|                                   |-------------------------------------------------------|
                         KC_LGUI, DEL_ALT,   LW_MHEN,  KC_SPC, KC_MS_BTN1,             KC_MS_BTN2,  KC_ENT, RS_HENK, KC_BSPC,  KC_ESC,
-                                                                 KC_PGUP, KC_MS_BTN3,  KC_PGDOWN, XXXXXXX, XXXXXXX, XXXXXXX
+                                                                 KC_PGUP, KC_MS_BTN3,    KC_PGDN, XXXXXXX, XXXXXXX, XXXXXXX
                                                             //`--------------'  `--------------'
     ),
   [_LOWER] = LAYOUT(
@@ -68,19 +68,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_LSFT,  KC_GRV, KC_TILD, KC_NUBS, KC_PIPE, XXXXXXX,                                        KC_EQL, KC_PLUS, KC_LABK, KC_RABK, KC_QUES, KC_UNDS,
   //|-------------------------------------------------------|                                   |-------------------------------------------------------|
                         KC_LGUI, DEL_ALT, KC_TRNS,  KC_SPC,   KC_MS_BTN4,             KC_MS_BTN5,  KC_ENT,   TT(3), KC_BSPC,  KC_ESC,
-                                                                 KC_PGUP, KC_MS_BTN3,  KC_PGDOWN, XXXXXXX, XXXXXXX, XXXXXXX
+                                                                 KC_PGUP, KC_MS_BTN3,    KC_PGDN, XXXXXXX, XXXXXXX, XXXXXXX
                                                             //`--------------'  `--------------'
     ),
   [_RAISE] = LAYOUT(
   //|-------------------------------------------------------|                                   |-------------------------------------------------------|
        KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                          KC_6,    KC_7,    KC_8,    KC_9,   KC_0,  KC_BSPC,
   //|-------------------------------------------------------|                                   |-------------------------------------------------------|
-      KC_LCTL,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                                        KC_APP,   KC_UP,S(KC_RO), KC_UNDS, KC_DQUO, KC_COLN,
+      KC_LCTL,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                                        KC_APP,   KC_UP,S(KC_INT1), KC_UNDS, KC_DQUO, KC_COLN,
   //|-------------------------------------------------------|                                   |-------------------------------------------------------|
       KC_LSFT,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,                                       KC_LEFT, KC_DOWN, KC_RGHT,  KC_DOT, KC_SLSH, KC_MINS,
   //|-------------------------------------------------------|                                   |-------------------------------------------------------|
                         KC_LGUI, DEL_ALT,   TT(3),  KC_SPC,   KC_MS_BTN4,             KC_MS_BTN5,  KC_ENT, KC_TRNS, KC_BSPC,  KC_ESC,
-                                                                 KC_PGUP, KC_MS_BTN3,  KC_PGDOWN, XXXXXXX, XXXXXXX, XXXXXXX
+                                                                 KC_PGUP, KC_MS_BTN3,    KC_PGDN, XXXXXXX, XXXXXXX, XXXXXXX
                                                             //`--------------'  `--------------'
     ),
   [_TRACKBALL] = LAYOUT(
@@ -92,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       XXXXXXX, XXXXXXX, RGB_VAD, RGB_SAD, RGB_HUD,RGB_RMOD,                                       SCRL_IN, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|-------------------------------------------------------|                                   |-------------------------------------------------------|
                         KC_LGUI, DEL_ALT, KC_TRNS,  KC_SPC,   KC_MS_BTN1,             KC_MS_BTN2,  KC_ENT, RS_HENK, KC_BSPC,  KC_ESC,
-                                                                 KC_PGUP, KC_MS_BTN3,  KC_PGDOWN, XXXXXXX, XXXXXXX, XXXXXXX
+                                                                 KC_PGUP, KC_MS_BTN3,    KC_PGDN, XXXXXXX, XXXXXXX, XXXXXXX
                                                             //`--------------'  `--------------'
     ),
   [_Layer4] = LAYOUT(
@@ -133,48 +133,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
 };
 
-keyevent_t encoder1_ccw = {
-    .key = (keypos_t){.row = 4, .col = 2},
-    .pressed = false
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+    [0] = { ENCODER_CCW_CW(KC_PGUP, KC_PGDN) },
+    [1] = { ENCODER_CCW_CW(KC_PGUP, KC_PGDN) },
+    [2] = { ENCODER_CCW_CW(KC_PGUP, KC_PGDN) },
+    [3] = { ENCODER_CCW_CW(KC_PGUP, KC_PGDN) },
+    [4] = { ENCODER_CCW_CW(KC_PGUP, KC_PGDN) },
+    [5] = { ENCODER_CCW_CW(KC_PGUP, KC_PGDN) },
+    [6] = { ENCODER_CCW_CW(KC_PGUP, KC_PGDN) },
 };
-
-keyevent_t encoder1_cw = {
-    .key = (keypos_t){.row = 4, .col = 5},
-    .pressed = false
-};
-
-bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) { /* First encoder */
-        if (clockwise) {
-            encoder1_cw.pressed = true;
-            encoder1_cw.time = (timer_read() | 1);
-            action_exec(encoder1_cw);
-        } else {
-            encoder1_ccw.pressed = true;
-            encoder1_ccw.time = (timer_read() | 1);
-            action_exec(encoder1_ccw);
-        }
-    }
-
-    return true;
-}
-
-
-void matrix_scan_user(void) {
-
-    if (IS_PRESSED(encoder1_ccw)) {
-        encoder1_ccw.pressed = false;
-        encoder1_ccw.time = (timer_read() | 1);
-        action_exec(encoder1_ccw);
-    }
-
-    if (IS_PRESSED(encoder1_cw)) {
-        encoder1_cw.pressed = false;
-        encoder1_cw.time = (timer_read() | 1);
-        action_exec(encoder1_cw);
-    }
-
-}
+#endif
 
 
 

keyboards/aki27/cocot46plus/matrix.c
@@ -1,3 +1,4 @@
+
 /*
 Copyright 2012-2018 Jun Wako, Jack Humbert, Yiancar
 
@@ -15,61 +16,44 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include "matrix.h"
-#include "quantum.h"
-
-#if (MATRIX_COLS <= 8)
-#    define print_matrix_header()  print("\nr/c 01234567\n")
-#    define print_matrix_row(row)  print_bin_reverse8(matrix_get_row(row))
-#    define matrix_bitpop(i)       bitpop(matrix[i])
-#    define ROW_SHIFTER ((uint8_t)1)
-#elif (MATRIX_COLS <= 16)
-#    define print_matrix_header()  print("\nr/c 0123456789ABCDEF\n")
-#    define print_matrix_row(row)  print_bin_reverse16(matrix_get_row(row))
-#    define matrix_bitpop(i)       bitpop16(matrix[i])
-#    define ROW_SHIFTER ((uint16_t)1)
-#elif (MATRIX_COLS <= 32)
-#    define print_matrix_header()  print("\nr/c 0123456789ABCDEF0123456789ABCDEF\n")
-#    define print_matrix_row(row)  print_bin_reverse32(matrix_get_row(row))
-#    define matrix_bitpop(i)       bitpop32(matrix[i])
-#    define ROW_SHIFTER  ((uint32_t)1)
-#endif
+#include "wait.h"
 
 static const pin_t row_pins[MATRIX_ROWS] = MATRIX_ROW_PINS;
 static const pin_t col_pins[MATRIX_COLS] = MATRIX_COL_PINS;
 
 static void select_row(uint8_t row)
 {
-    setPinOutput(row_pins[row]);
-    writePinLow(row_pins[row]);
+    gpio_set_pin_output(row_pins[row]);
+    gpio_write_pin_low(row_pins[row]);
 }
 
 static void unselect_row(uint8_t row)
 {
-    setPinInputHigh(row_pins[row]);
+    gpio_set_pin_input_high(row_pins[row]);
 }
 
 static void unselect_rows(void)
 {
     for(uint8_t x = 0; x < MATRIX_ROWS; x++) {
-        setPinInputHigh(row_pins[x]);
+        gpio_set_pin_input_high(row_pins[x]);
     }
 }
 
 static void select_col(uint8_t col)
 {
-    setPinOutput(col_pins[col]);
-    writePinLow(col_pins[col]);
+    gpio_set_pin_output(col_pins[col]);
+    gpio_write_pin_low(col_pins[col]);
 }
 
 static void unselect_col(uint8_t col)
 {
-    setPinInputHigh(col_pins[col]);
+    gpio_set_pin_input_high(col_pins[col]);
 }
 
 static void unselect_cols(void)
 {
     for(uint8_t x = 0; x < MATRIX_COLS; x++) {
-        setPinInputHigh(col_pins[x]);
+        gpio_set_pin_input_high(col_pins[x]);
     }
 }
 
@@ -77,10 +61,10 @@ static void init_pins(void) {
   unselect_rows();
   unselect_cols();
   for (uint8_t x = 0; x < MATRIX_COLS; x++) {
-    setPinInputHigh(col_pins[x]);
+    gpio_set_pin_input_high(col_pins[x]);
   }
   for (uint8_t x = 0; x < MATRIX_ROWS; x++) {
-    setPinInputHigh(row_pins[x]);
+    gpio_set_pin_input_high(row_pins[x]);
   }
 }
 
@@ -100,10 +84,10 @@ static bool read_cols_on_row(matrix_row_t current_matrix[], uint8_t current_row)
     for(uint8_t col_index = 0; col_index < MATRIX_COLS; col_index++) {
 
         // Select the col pin to read (active low)
-        uint8_t pin_state = readPin(col_pins[col_index]);
+        uint8_t pin_state = gpio_read_pin(col_pins[col_index]);
 
         // Populate the matrix row with the state of the col pin
-        current_matrix[current_row] |=  pin_state ? 0 : (ROW_SHIFTER << col_index);
+        current_matrix[current_row] |=  pin_state ? 0 : (MATRIX_ROW_SHIFTER << col_index);
     }
 
     // Unselect row
@@ -128,15 +112,15 @@ static bool read_rows_on_col(matrix_row_t current_matrix[], uint8_t current_col)
         matrix_row_t last_row_value = current_matrix[tmp];
 
         // Check row pin state
-        if (readPin(row_pins[row_index]) == 0)
+        if (gpio_read_pin(row_pins[row_index]) == 0)
         {
             // Pin LO, set col bit
-            current_matrix[tmp] |= (ROW_SHIFTER << current_col);
+            current_matrix[tmp] |= (MATRIX_ROW_SHIFTER << current_col);
         }
         else
         {
             // Pin HI, clear col bit
-            current_matrix[tmp] &= ~(ROW_SHIFTER << current_col);
+            current_matrix[tmp] &= ~(MATRIX_ROW_SHIFTER << current_col);
         }
 
         // Determine if the matrix changed state

keyboards/aki27/cocot46plus/rules.mk
@@ -1,33 +1,7 @@
-# MCU name
-MCU = atmega32u4
-
-# Bootloader selection
-BOOTLOADER = caterina
-
-# Build Options
-#   change yes to no to disable
-#
-ENCODER_ENABLE = yes
-BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = yes       # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no         # Commands for debug and configuration
-# if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-NKRO_ENABLE = no            # USB Nkey Rollover
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
-AUDIO_ENABLE = no           # Audio output
-LTO_ENABLE = yes
-
 CUSTOM_MATRIX = lite
 SRC += matrix.c
 
-POINTING_DEVICE_ENABLE = yes
 POINTING_DEVICE_DRIVER = adns5050
-
-OLED_ENABLE     = yes
-OLED_DRIVER     = SSD1306
 
 # Disable unnecessary functions to reduce firmware size
 SPACE_CADET_ENABLE = no

keyboards/aki27/trackball_module/config.h
@@ -17,41 +17,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#include "config_common.h"
-
-/* USB Device descriptor parameter */
-#define VENDOR_ID    0x1727
-#define PRODUCT_ID   0x0004
-#define DEVICE_VER   0x0001
-#define MANUFACTURER aki27
-#define PRODUCT      trackball_module
+#define DYNAMIC_KEYMAP_LAYER_COUNT 7
 
 /* key matrix size */
 #define MATRIX_ROWS 1
-#define MATRIX_COLS 5
-
-// wiring of each half
-#define DIRECT_PINS { { B6, B3, B2, NO_PIN, NO_PIN } }
-#define UNUSED_PINS { F4, F5, D4, C6, D7, E6, B4, B5}
-
-/* Set 0 if debouncing isn't needed */
-#define DEBOUNCE 5
-
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#undef LOCKING_SUPPORT_ENABLE
-/* Locking resynchronize hack */
-#undef LOCKING_RESYNC_ENABLE
-
-/* Limit the maximum layer number to 8 */
-#define LAYER_STATE_8BIT
-
-/* Rotary encoder */
-#define ENCODERS_PAD_A { D2 }
-#define ENCODERS_PAD_B { D3 }
-#define ENCODER_RESOLUTION 4
-
-#define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 200
+#define MATRIX_COLS 3
 
 #define ADNS5050_SCLK_PIN         F7
 #define ADNS5050_SDIO_PIN         F6

keyboards/aki27/trackball_module/keymaps/default/keymap.c
@@ -32,62 +32,28 @@ enum layer_number {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
-       KC_MS_BTN1,    KC_MS_BTN2,    KC_MS_BTN3,    KC_PGUP,    KC_PGDOWN
+       KC_MS_BTN1,    KC_MS_BTN2,    KC_MS_BTN3
     ),
   [_LOWER] = LAYOUT(
-       KC_A,    KC_B,    KC_C,    KC_D,    KC_E
+       KC_A,    KC_B,    KC_C
     ),
   [_RAISE] = LAYOUT(
-       KC_A,    KC_B,    KC_C,    KC_D,    KC_E
+       KC_A,    KC_B,    KC_C
     ),
   [_TRACKBALL] = LAYOUT(
-       KC_A,    KC_B,    KC_C,    KC_D,    KC_E
+       KC_A,    KC_B,    KC_C
     )
 };
 
 
-keyevent_t encoder1_ccw = {
-    .key = (keypos_t){.row = 0, .col = 3},
-    .pressed = false
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+    [0] = { ENCODER_CCW_CW(KC_PGUP, KC_PGDN) },
+    [1] = { ENCODER_CCW_CW(KC_PGUP, KC_PGDN) },
+    [2] = { ENCODER_CCW_CW(KC_PGUP, KC_PGDN) },
+    [3] = { ENCODER_CCW_CW(KC_PGUP, KC_PGDN) },
 };
-
-keyevent_t encoder1_cw = {
-    .key = (keypos_t){.row = 0, .col = 4},
-    .pressed = false
-};
-
-bool encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) { // First encoder 
-        if (clockwise) {
-            encoder1_cw.pressed = true;
-            encoder1_cw.time = (timer_read() | 1);
-            action_exec(encoder1_cw);
-        } else {
-            encoder1_ccw.pressed = true;
-            encoder1_ccw.time = (timer_read() | 1);
-            action_exec(encoder1_ccw);
-        }
-    }
-
-    return true;
-}
-
-
-void matrix_scan_user(void) {
-
-    if (IS_PRESSED(encoder1_ccw)) {
-        encoder1_ccw.pressed = false;
-        encoder1_ccw.time = (timer_read() | 1);
-        action_exec(encoder1_ccw);
-    }
-
-    if (IS_PRESSED(encoder1_cw)) {
-        encoder1_cw.pressed = false;
-        encoder1_cw.time = (timer_read() | 1);
-        action_exec(encoder1_cw);
-    }
-
-}
+#endif
 
 
 

yoichi/qmk_firmware_aki27kbd

commit e303535b90bfaaf548a8f9026f3fdc27b453b0f6

Fix scrl_inv handling
e6074e96357eca4be6104592224453ba5dbd7a19 caused the condition to always be true

keyboards/aki27/cocot46plus/cocot46plus.c
@@ -104,7 +104,7 @@ report_mouse_t pointing_device_task_kb(report_mouse_t mouse_report) {
         }
 
         // Accumulate scroll values
-        if (cocot_config.scrl_inv) {
+        if (cocot_config.scrl_inv > 0) {
             h_acm += smoothed_x;
             v_acm -= smoothed_y;
         } else {